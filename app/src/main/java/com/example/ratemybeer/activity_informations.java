cd1c374e17189992272821a52a331687d7c514f2
badreddine2 <badreddine.aji@gmail.com>
2021-03-06 09:58:54 +0100

    ok1
    Signed-off-by: badreddine2 <badreddine.aji@gmail.com>

---
+++ app/src/main/java/com/example/ratemybeer/activity_informations.java
@@ -0,0 +1,14 @@
+package com.example.ratemybeer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_informations extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_informations);
+    }
+}
