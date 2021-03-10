41d68dcfbaf0c75729048711e124c5fb6aa3528d
Antoine <antoinehoe@gmail.com>
2021-03-10 20:20:33 +0100

    ajout variable global_rating + getter & setters

--- app/src/main/java/com/example/ratemybeer/Biere.java
+++ app/src/main/java/com/example/ratemybeer/Biere.java
@@ -3,14 +3,16 @@ package com.example.ratemybeer;
 public class Biere {
     private int image;
     private String name;
+    private float global_rating;
     private String description;
 
     public Biere(){};
 
-    public Biere(String name, String description){
+    public Biere(String name, String description, float global_rating){
 
         this.name=name;
         this.description=description;
+        this.global_rating = global_rating;
     }
 
     public String getDescription() {
@@ -25,4 +27,23 @@ public class Biere {
         return name;
     }
 
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getGlobal_rating() {
+        return global_rating;
+    }
+
+    public void setGlobal_rating(float global_rating) {
+        this.global_rating = global_rating;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
