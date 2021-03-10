1d3e0a2d4a22f6f1e9c4a1345549c9ba22f1bef2
badreddine2 <badreddine.aji@gmail.com>
2021-03-10 18:40:25 +0100

    rating
    Signed-off-by: badreddine2 <badreddine.aji@gmail.com>

--- app/src/main/java/com/example/ratemybeer/Activity_informations.java
+++ app/src/main/java/com/example/ratemybeer/Activity_informations.java
@@ -2,19 +2,45 @@ package com.example.ratemybeer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-
-import com.example.ratemybeer.ui.login.LoginActivity;
+import android.widget.RatingBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class Activity_informations extends AppCompatActivity {
-
+     TextView tv,tv1 ;
+     RatingBar rb ;
+     SeekBar sb ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informations);
+        tv = findViewById(R.id.tv) ;
+        tv1 = findViewById(R.id.tv1) ;
+        rb = findViewById(R.id.rb) ;
+        sb = findViewById(R.id.sb) ;
+        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                tv.setText("Value is :" +progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        rb.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                tv1.setText("rating is : "+rating);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/ratemybeer/BeerActivity.java
+++ app/src/main/java/com/example/ratemybeer/BeerActivity.java
@@ -6,13 +6,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class BeerActivity extends AppCompatActivity {
-
+    TextView vs ;
+    RatingBar rb ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beer);
+        vs= findViewById(R.id.vs) ;
+        rb = findViewById(R.id.rb) ;
         final Button retour=findViewById(R.id.buttonRetourFromBeerActivity);
         retour.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -20,6 +25,12 @@ public class BeerActivity extends AppCompatActivity {
                 Intent otherActivity=new Intent(getApplicationContext(),TimelineActivity.class);
                 startActivity(otherActivity);
                 finish();
+            }
+        });
+        rb.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                vs.setText("Your rating is : "+rating);
             }
         });
     }
