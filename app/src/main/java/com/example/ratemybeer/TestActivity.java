f3aa075be863c3cdebbe84225531002635e9827c
Antoine <antoinehoe@gmail.com>
2021-03-15 20:26:05 +0100

    amelioration vue detail biere

--- app/src/main/java/com/example/ratemybeer/TestActivity.java
+++ app/src/main/java/com/example/ratemybeer/TestActivity.java
@@ -18,6 +18,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -73,6 +74,7 @@ public class TestActivity extends AppCompatActivity {
 
             }
         });
+
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
